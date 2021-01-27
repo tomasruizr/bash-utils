db366697af2c163b1a95ee9ef1c7897d5a3024cf
Tomás Ruiz <tomasruizr@gmail.com>
2021-01-27 17:01:00 -0300

    [add] docker install scripts

---
+++ centosInstallDocker.sh
@@ -0,0 +1,7 @@
+sudo yum check-update
+curl -fsSL https://get.docker.com/ | sh
+sudo systemctl start docker
+sudo systemctl status docker
+curl -L `curl -fsSLI -o /dev/null -w %{url_effective} https://github.com/docker/compose/releases/latest`/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
---
+++ debianInstallDocker.sh
@@ -0,0 +1,10 @@
+apt update
+apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt update
+apt-cache policy docker-ce
+apt install -y docker-ce
+curl -L `curl -fsSLI -o /dev/null -w %{url_effective} https://github.com/docker/compose/releases/latest`/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
